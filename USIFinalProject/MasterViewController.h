73ad4de93dbe6f31549f619af22ea5395d5857a9
BigBam <bamwebsites@gmail.com>
2014-01-08 14:35:24 -0500

    Initial commit

---
+++ USIFinalProject/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  USIFinalProject
+//
+//  Created by Bam Faboyede on 1/8/14.
+//  Copyright (c) 2014 Bam F. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
